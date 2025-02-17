75ba285981ef22e74708e97c71e02a263c103c01
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-02-17 20:28:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -302,13 +302,14 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply(
-        text=FORCE_MSG.format(
-            first=message.from_user.first_name,
-            last=message.from_user.last_name,
-            username=None if not message.from_user.username else '@' + message.from_user.username,
-            mention=message.from_user.mention,
-            id=message.from_user.id
+    await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
         ),
         reply_markup=InlineKeyboardMarkup(buttons),
         quote=True,
