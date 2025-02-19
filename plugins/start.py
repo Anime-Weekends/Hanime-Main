a803c2f2461c0dd25217cc983266805fed8e811a
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-02-19 18:34:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -223,9 +223,8 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_photo(
-            photo=random.PICS),
-            caption=START_MSG.format(
+        await message.reply_text(
+            text=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
@@ -303,9 +302,8 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply_photo(
-            photo=random.PICS),
-            caption=START_MSG.format(
+    await message.reply_text(
+            text=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
