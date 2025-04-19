6f051c9575e388b93ff85d7fd72b77ed02798e71
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-19 11:56:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -224,7 +224,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_text(
-            caption=START_MSG.format(
+            text=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
@@ -292,7 +292,7 @@ async def not_joined(client: Client, message: Message):
         ]
     try:
         buttons.append(
-            []
+            [
                 InlineKeyboardButton(
                     text='Try Again 🥺',
                     url=f"https://t.me/{client.username}?start={message.command[1]}"
